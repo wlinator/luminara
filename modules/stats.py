eb87e658a18cc95ed5bfd260bce5eb84b88b7c5a
wlinator <git@wlinator.org>
2023-06-21 08:20:49 -0400

    Stats rewrite

--- modules/stats.py
+++ modules/stats.py
@@ -31,31 +31,41 @@ class Stats(commands.Cog):
     )
     # @commands.check(universal.channel_check)
     @commands.check(universal.beta_check)
-    async def all(self, ctx):
-        # collect data
-        bj_games_amount = BlackJackStats.count_games()
-        (bj_winning_games_amount, bj_losing_games_amount) = BlackJackStats.get_winning_and_losing_amount()
-        (bj_total_investment, bj_total_payout) = BlackJackStats.get_investment_and_payout()
+    async def all(self, ctx, type: discord.Option(choices=["BlackJack"])):
+        if type == "BlackJack":
+            # collect data
+            bj_games_amount = BlackJackStats.count_games()
+            (bj_winning_games_amount, bj_losing_games_amount) = BlackJackStats.get_winning_and_losing_amount()
+            (bj_total_investment, bj_total_payout) = BlackJackStats.get_investment_and_payout()
 
-        # calculate data
-        roi = ((bj_total_payout - bj_total_investment) / bj_total_investment) * 100
-        roi = round(roi, 3)
+            # calculate data
+            roi = ((bj_total_payout - bj_total_investment) / bj_total_investment) * 100
+            roi = round(roi, 3)
 
-        # output
-        embed = discord.Embed(
-            title=strings["stats_all_title"]
-        )
-        embed.add_field(name=strings["stats_all_blackjack"],
-                        value=strings["stats_all_gambling_value3"].format(
-                            bj_games_amount,
-                            bj_winning_games_amount,
-                            bj_losing_games_amount,
-                            round(bj_total_investment),
-                            round(bj_total_payout),
-                            roi
-                        ))
+            # output
+            embed = discord.Embed(
+                title=strings["stats_all_title"].format("BlackJack")
+            )
+            embed.add_field(inline=False,
+                            name=strings["stats_games"].format(bj_games_amount),
+                            value=strings["stats_games_value"].format(
+                                bj_winning_games_amount,
+                                bj_losing_games_amount
+                            ))
+            embed.add_field(inline=False,
+                            name=strings["stats_cashflow"],
+                            value=strings["stats_cashflow_value"].format(
+                                round(bj_total_investment),
+                                round(bj_total_payout)
+                            ))
+            embed.add_field(inline=False,
+                            name=strings["stats_roi"],
+                            value=strings["stats_roi_value"].format(
+                                roi
+                            ))
+            embed.set_footer(text=strings["stats_all_footer"])
 
-        await ctx.respond(embed=embed)
+            await ctx.respond(embed=embed)
 
     @stats.command(
         name="me",
