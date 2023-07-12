0f95c05afe1125309402534b72225b40653f28b2
dok4440 <dokimakimaki@gmail.com>
2023-07-12 10:28:52 -0400

    Testing leaderboard v2

--- modules/leaderboard.py
+++ modules/leaderboard.py
@@ -16,6 +16,66 @@ class LeaderboardCog(commands.Cog):
         self.bot = sbbot
 
     @commands.slash_command(
+        name="leaderboardv2",
+        description="Are ya winning' son?",
+        guild_only=True
+    )
+    @commands.check(universal.beta_check)
+    async def leaderboardv2(self, ctx):
+        xp_lb = Xp.load_leaderboard()
+        cash_lb = Currency.load_leaderboard()
+
+        embed = discord.Embed(
+            color=discord.Color.embed_background(),
+        )
+        embed.set_author(name="Rave Cave Leaderboard")
+        embed.set_thumbnail(url="https://i.imgur.com/79XfsbS.png")
+        embed.set_footer(text=f"Do /level to see your rank.")
+
+        # XP FIELD
+        value = ""
+        for i, (user_id, xp, level, rank, xp_needed_for_next_level) in enumerate(xp_lb[:5], start=1):
+            try:
+                member = await ctx.guild.fetch_member(user_id)
+                name = member.name
+
+            except Exception as error:
+                name = "Unknown User"
+                racu_logs.debug(f"Currency Leaderboard: Unknown User, {error}")
+
+            value += f"Lv. {level} - {name}\n"
+
+        embed.add_field(
+            name="Top Five Chatters",
+            value=value,
+            inline=False
+        )
+
+        # CURRENCY FIELD
+        value = ""
+        for i, (user_id, cash_balance, rank) in enumerate(cash_lb[:5], start=1):
+            try:
+                member = await ctx.guild.fetch_member(user_id)
+                name = member.name
+
+            except Exception as error:
+                name = "Unknown User"
+                racu_logs.debug(f"Currency Leaderboard: Unknown User, {error}")
+
+            locale.setlocale(locale.LC_ALL, '')
+            cash_balance = locale.format_string("%d", cash_balance, grouping=True)
+
+            value += f"${cash_balance} - {name}\n"
+
+        embed.add_field(
+            name=f"Top Five Wealth",
+            value=value,
+            inline=False
+        )
+
+        await ctx.respond(embed=embed)
+
+    @commands.slash_command(
         name="leaderboard",
         description="Are ya winnin' son?",
         guild_only=True
