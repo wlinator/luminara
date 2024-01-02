7f07cbc7c03b9d782e791462a93801af602c736d
wlinator <git@wlinator.org>
2024-01-02 14:29:04 +0100

    Add ban and unban logs

--- modules/logging.py
+++ modules/logging.py
@@ -3,14 +3,16 @@ import logging
 from discord.ext import commands
 import discord
 
-from main import strings
 from sb_tools import universal
+from main import economy_config
 import pytz
 from datetime import datetime
 
 racu_logs = logging.getLogger('Racu.Core')
 est = pytz.timezone('US/Eastern')
+
 logging_channel_id = 1191707328148418611
+rc_guild_id = economy_config["rc_guild_id"]
 
 
 class LoggingCog(commands.Cog):
@@ -19,6 +21,10 @@ class LoggingCog(commands.Cog):
     
     @commands.Cog.listener()
     async def on_message_delete(self, message):
+
+        if message.guild.id != rc_guild_id:
+            return
+
         author = message.author
         content = message.clean_content
 
@@ -34,12 +40,12 @@ class LoggingCog(commands.Cog):
             attachment_list += f"{attachment.content_type} | {attachment.proxy_url}\n"
 
         embed = discord.Embed(
-            color = discord.Color.red(),
+            color = discord.Color.orange(),
             description = f"**Message sent by {author.mention} deleted in <#{message.channel.id}>.**"
         )
 
         embed.set_author(name=author.name, icon_url=author.display_avatar)
-        embed.add_field(name="Message ID", value=f"```\n{message.id}\n```")
+        embed.add_field(name="Message ID", value=f"```\n{message.id}\n```", inline=False)
         embed.add_field(name="Content", value=f"```\n{content}\n```", inline=False)
 
         if attachments:
@@ -48,6 +54,66 @@ class LoggingCog(commands.Cog):
         embed.set_footer(text=formatted_time)
 
         await message.guild.get_channel(logging_channel_id).send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after):
+
+        if after.guild.id != rc_guild_id:
+            return
+
+        author = after.author # same as before.author
+        edit_time = datetime.now(est).strftime("%Y-%m-%d %I:%M %p")
+
+        embed = discord.Embed(
+            color = discord.Color.yellow(),
+            description = f"**Message sent by {author.mention} edited in <#{after.channel.id}>.**"
+        )
+
+        embed.set_author(name=author.name, icon_url = author.display_avatar)
+        embed.add_field(name="Message ID", value=f"```\n{after.id}\n```", inline=False)
+        embed.add_field(name="Before", value=f"```\n{before.clean_content}\n```", inline=False)
+        embed.add_field(name="After", value=f"```\n{after.clean_content}\n```", inline=False)
+        embed.set_footer(text=edit_time)
+
+        await after.guild.get_channel(logging_channel_id).send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_member_ban(self, guild, user):
+
+        if guild.id != rc_guild_id:
+            return
+
+        current_time = datetime.now(est).strftime("%Y-%m-%d %I:%M %p")
+
+        embed = discord.Embed(
+            color = discord.Color.red(),
+            title = f"**User @{user.name} was banned from the server.**"
+        )
+        embed.add_field(name="User ID", value=f"```\n{user.id}\n```")
+        embed.set_thumbnail(url=user.display_avatar)
+        embed.set_footer(text=current_time)
+
+        await guild.get_channel(logging_channel_id).send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_member_unban(self, guild, user):
+
+        if guild.id != rc_guild_id:
+            return
+
+        current_time = datetime.now(est).strftime("%Y-%m-%d %I:%M %p")
+
+        embed = discord.Embed(
+            color = discord.Color.red(),
+            title = f"**User @{user.name} was unbanned from the server.**"
+        )
+        embed.add_field(name="User ID", value=f"```\n{user.id}\n```")
+        embed.set_thumbnail(url=user.display_avatar)
+        embed.set_footer(text=current_time)
+
+        await guild.get_channel(logging_channel_id).send(embed=embed)
+
+
 
 
 def setup(sbbot):
