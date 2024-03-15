31c22610ebaf0912a3a38a2f9e19e78ded70c969
wdb <dokimakimaki@gmail.com>
2024-03-16 00:04:31 +0100

    Fix lib/checks to check guild_config.commands_channel

--- lib/checks.py
+++ lib/checks.py
@@ -24,26 +24,26 @@ async def birthday_module(ctx):
 
 
 async def channel(ctx):
-    desired_channel_id = 1118587309365940407  # bot-chat in RCU
-    owner_id = os.getenv("OWNER_ID")
+    guild_config = GuildConfig(ctx.guild.id)
+    command_channel_id = guild_config.command_channel_id
 
-    if ctx.channel.id != desired_channel_id and ctx.author.id != int(owner_id):
-        channel_mention = f"<#{desired_channel_id}>"
-        await ctx.respond(f"You can only do that command in {channel_mention}.", ephemeral=True)
-        return False
+    if command_channel_id:
+        if ctx.channel.id != command_channel_id:
 
-    return True
+            try:
+                command_channel = await ctx.guild.fetch_channel(command_channel_id)
+                await ctx.respond(f"You can only do that command in {command_channel.mention}.", ephemeral=True)
+                return False
 
+            except (discord.HTTPException, discord.NotFound):
+                return True
 
-async def beta_command(ctx):
-    owner_id = os.getenv("OWNER_ID")
-    if ctx.author.id != int(owner_id):
-        embed = discord.Embed(description=f"You can't use this command just yet! It's currently undergoing testing and "
-                                          f"fine-tuning to ensure the best experience for all users. Stay tuned for its"
-                                          f" official release.",
-                              color=discord.Color.red())
-        await ctx.respond(embed=embed, ephemeral=True)
-        return False
+            except discord.Forbidden:
+                await ctx.respond(f"I don't have sufficient permissions to check "
+                                  f"whether this channel allows commands or not. "
+                                  f"Please do '/config commands channel <channel>' with a channel "
+                                  f"I can see or allow commands everywhere.")
+                return False
 
     return True
 
@@ -54,19 +54,6 @@ async def bot_owner(ctx):
         embed = discord.Embed(description=f"This command requires bot admin permissions.",
                               color=discord.Color.red())
         await ctx.respond(embed=embed, ephemeral=True)
-        return False
-
-    return True
-
-
-async def eightball(message):
-    desired_channel_id = 1118587309365940407
-
-    if message.channel.id != desired_channel_id:
-        channel_mention = f"<#{desired_channel_id}>"
-        bot_reply = await message.reply(f"You can only ask Racu questions in {channel_mention}.")
-        await asyncio.sleep(5)
-        await bot_reply.delete()
         return False
 
     return True
