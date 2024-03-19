8a3c78232f455ce2cc0aeaa61c714f306a42f5b5
wlinator <git@wlinator.org>
2024-03-19 08:46:52 +0100

    Create ErrorHandler to handle both prefix and slash errors

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from lib.embeds.greet import Greet
 from config import json_loader
 from handlers.ReactionHandler import ReactionHandler
 from handlers.XPHandler import XPHandler
-from handlers import LoggingHandler
+from handlers import LoggingHandler, ErrorHandler
 from services.GuildConfig import GuildConfig
 from services.Help import RacuHelp
 
@@ -94,7 +94,7 @@ async def on_member_join(member):
         logs.info(f"[GreetingHandler] Message not sent in '{member.guild.name}'. Channel ID may be invalid. {e}")
 
 
-@client.event
+@client.listen()
 async def on_command_completion(ctx) -> None:
     """
     This code is executed when a slash_command has been successfully executed.
@@ -120,46 +120,19 @@ async def on_command_completion(ctx) -> None:
         logs.info(f"[CommandHandler] {ctx.author.name} successfully did \"/{executed_command}\". | direct message")
 
 
-@client.event
+@client.listen()
 async def on_command_error(ctx, error) -> None:
-    if isinstance(error, commands.CommandOnCooldown):
+    await ErrorHandler.on_command_error(ctx, error)
 
-        seconds = error.retry_after
-        minutes = seconds // 60
-        seconds %= 60
-        cooldown = "{:02d}:{:02d}".format(int(minutes), int(seconds))
 
-        await ctx.respond(embed=GenericErrors.command_on_cooldown(ctx, cooldown))
-
-    elif isinstance(error, commands.MissingPermissions):
-        await ctx.respond(embed=GenericErrors.missing_permissions(ctx))
-
-    elif isinstance(error, commands.BotMissingPermissions):
-        await ctx.respond(embed=GenericErrors.bot_missing_permissions(ctx))
-
-    elif isinstance(error, commands.PrivateMessageOnly):
-        await ctx.respond(embed=GenericErrors.private_message_only(ctx))
-
-    elif isinstance(error, commands.NoPrivateMessage):
-        await ctx.respond(embed=GenericErrors.guild_only(ctx))
-
-    elif isinstance(error, (discord.CheckFailure, commands.CheckFailure)):
-        logs.info(f"[CommandHandler] {ctx.author.name} check failure: \"/{ctx.command.qualified_name}\"")
-
-    elif isinstance(error, (commands.MissingRequiredArgument, commands.BadArgument, commands.CommandNotFound)):
-        pass
-
-    else:
-        await ctx.respond(embed=GenericErrors.default_exception(ctx))
-        traceback.print_tb(error.original.__traceback__)
-
-    logs.error(f"[CommandHandler] on_command_error: {error}")
+@client.listen()
+async def on_application_command_error(ctx, error) -> None:
+    await ErrorHandler.on_command_error(ctx, error)
 
 
 @client.event
 async def on_error(event: str, *args, **kwargs) -> None:
-    logs.error(f"[EventHandler] on_error INFO: errors.event.{event} | '*args': {args} | '**kwargs': {kwargs}")
-    logs.error(f"[EventHandler] on_error EXCEPTION: {sys.exc_info()}")
+    await ErrorHandler.on_error(event, *args, **kwargs)
 
 
 # load all json
