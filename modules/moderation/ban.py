5cc4d2a371a0d07631dbe2b5d08e76476ea64cd8
dok4440 <dokimakimaki@gmail.com>
2024-03-29 13:15:42 -0400

    Add shorten() to lib.formatter

--- modules/moderation/ban.py
+++ modules/moderation/ban.py
@@ -2,7 +2,8 @@ import logging
 
 import discord
 from modules.moderation import functions
-from lib.embeds.moderation import ModEmbeds, ModErrors, shorten
+from lib.embeds.moderation import ModEmbeds, ModErrors
+from lib import formatter
 
 
 _logs = logging.getLogger('Racu.Core')
@@ -31,12 +32,12 @@ async def ban_user(cog, ctx, target: discord.User, reason):
         member_name = member.name
         member_id = member.id
 
-        await member.ban(reason=f"moderator: {ctx.author.name} | reason: {shorten(reason, 200)}")
+        await member.ban(reason=f"moderator: {ctx.author.name} | reason: {formatter.shorten(reason, 200)}")
         return await ctx.respond(embed=ModEmbeds.member_banned(ctx, member_name, member_id, reason, dm_sent))
 
     # not a member in this guild, so ban right away
     else:
-        await ctx.guild.ban(target, reason=f"moderator: {ctx.author.name} | reason: {shorten(reason, 200)}")
+        await ctx.guild.ban(target, reason=f"moderator: {ctx.author.name} | reason: {formatter.shorten(reason, 200)}")
         return await ctx.respond(embed=ModEmbeds.user_banned(ctx, target.id, reason))
 
 
@@ -46,7 +47,7 @@ async def unban_user(ctx, target: discord.User, reason):
         reason = "No reason provided."
 
     try:
-        await ctx.guild.unban(target, reason=f"moderator: {ctx.author.name} | reason: {shorten(reason, 200)}")
+        await ctx.guild.unban(target, reason=f"moderator: {ctx.author.name} | reason: {formatter.shorten(reason, 200)}")
         return await ctx.respond(embed=ModEmbeds.user_unban(ctx, target.id))
 
     except (discord.NotFound, discord.HTTPException):
