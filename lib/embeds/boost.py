6be17c925c2764a4a4fff56f56dcfa48df13c8d0
wlinator <dokimakimaki@gmail.com>
2024-04-26 08:01:12 -0400

    Change the content of the booster embed.

--- lib/embeds/boost.py
+++ lib/embeds/boost.py
@@ -13,6 +13,7 @@ class Boost:
     def message(member, template=None):
         embed = discord.Embed(
             color=discord.Color.nitro_pink(),
+            title="New Booster",
             description=f"_ _\nThanks for boosting, **{member.name}**!!"
         )
 
@@ -20,6 +21,7 @@ class Boost:
             # REPLACE
             embed.description = lib.formatter.template(template, member.name)
 
+        embed.set_author(name=member.name, icon_url=member.display_avatar)
         embed.set_thumbnail(url=boost_icon)
         embed.set_footer(text=f"Total server boosts: {member.guild.premium_subscription_count}",
                          icon_url=exclam_icon)
