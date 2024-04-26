746daac49f4db2c9226d4bc7963346a81783b730
wlinator <dokimakimaki@gmail.com>
2024-04-26 07:13:58 -0400

    First boost test push

---
+++ lib/embeds/boost.py
@@ -0,0 +1,27 @@
+import discord
+
+from config.parser import JsonCache
+import lib.formatter
+
+resources = JsonCache.read_json("art")
+exclam_icon = resources["icons"]["exclam"]
+boost_icon = resources["icons"]["boost"]
+
+
+class Boost:
+    @staticmethod
+    def message(member, template=None):
+        embed = discord.Embed(
+            color=discord.Color.embed_background(),
+            description=f"_ _\nThanks for boosting, **{member.name}**!!"
+        )
+
+        if template:
+            # REPLACE
+            embed.description = lib.formatter.template(template, member.name)
+
+        embed.set_thumbnail(url=boost_icon)
+        embed.set_footer(text=f"Total server boosts: {member.guild.premium_subscription_count}",
+                         icon_url=exclam_icon)
+
+        return embed
