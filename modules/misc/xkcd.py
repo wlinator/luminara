da9ae6d8075c832f15c9284a4109ba34d80e9c4d
wlinator <git@wlinator.org>
2024-06-16 20:40:39 +0200

    Allow xkcd command everywhere and clean up

--- modules/misc/xkcd.py
+++ modules/misc/xkcd.py
@@ -1,23 +1,18 @@
-import logging
-
-import httpx
-
 from discord.ext import commands
 from services import xkcd_service
 from lib.embeds.info import MiscInfo
 
-_xkcdc = xkcd_service.Client()
-_logs = logging.getLogger('Lumi.Core')
+_xkcd = xkcd_service.Client()
 
 
 async def print_comic(ctx, latest=False, number: int = None):
     try:
         if latest:
-            comic = _xkcdc.get_latest_comic(raw_comic_image=True)
+            comic = _xkcd.get_latest_comic(raw_comic_image=True)
         elif number is not None:
-            comic = _xkcdc.get_comic(number, raw_comic_image=True)
+            comic = _xkcd.get_comic(number, raw_comic_image=True)
         else:
-            comic = _xkcdc.get_random_comic(raw_comic_image=True)
+            comic = _xkcd.get_random_comic(raw_comic_image=True)
 
         description = f"[Explainxkcd]({comic.explanation_url}) | [Webpage]({comic.comic_url})"
         embed = MiscInfo.xkcd(comic.id, comic.title, description, comic.image_url)
