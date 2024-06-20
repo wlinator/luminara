066eb4ceae16d892c4447c8dfe3ef72eef1b0da5
wlinator <dokimakimaki@gmail.com>
2024-06-20 19:01:15 +0200

    Fix typo globally

--- lib/embeds/boost.py
+++ lib/embeds/boost.py
@@ -4,7 +4,7 @@ from config.parser import JsonCache
 import lib.formatter
 
 resources = JsonCache.read_json("art")
-exclam_icon = resources["icons"]["exclam"]
+exclaim_icon = resources["icons"]["exclaim"]
 boost_icon = resources["icons"]["boost"]
 
 
@@ -24,6 +24,6 @@ class Boost:
         embed.set_author(name=member.name, icon_url=member.display_avatar)
         embed.set_image(url=image_url if image_url else boost_icon)
         embed.set_footer(text=f"Total server boosts: {member.guild.premium_subscription_count}",
-                         icon_url=exclam_icon)
+                         icon_url=exclaim_icon)
 
         return embed
