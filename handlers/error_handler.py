551baeb6d6ccf597eb4ed4737719d23bb2d0a75a
wlinator <git@wlinator.org>
2024-07-02 20:46:55 +0200

    chore: Refactor error handling functions and add error listener

--- handlers/error_handler.py
+++ handlers/error_handler.py
@@ -1,114 +1,100 @@
 import sys
 import traceback
 from loguru import logger
-
-import discord
 from discord.ext import commands
-from discord.ext.commands import Cog
 
 from lib.embeds.error import GenericErrors, BdayErrors
 from lib.exceptions import LumiExceptions
 
 
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        pass
-
-    elif isinstance(error, commands.CommandOnCooldown):
-        seconds = error.retry_after
-        minutes = seconds // 60
-        seconds %= 60
-        cooldown = "{:02d}:{:02d}".format(int(minutes), int(seconds))
-
-        await ctx.respond(embed=GenericErrors.command_on_cooldown(ctx, cooldown))
-
-    elif isinstance(error, commands.MissingPermissions):
-        await ctx.respond(embed=GenericErrors.missing_permissions(ctx))
-
-    elif isinstance(error, commands.BotMissingPermissions):
-        await ctx.respond(embed=GenericErrors.bot_missing_permissions(ctx))
-
-    elif isinstance(error, commands.PrivateMessageOnly):
-        await ctx.respond(embed=GenericErrors.private_message_only(ctx))
-
-    elif isinstance(error, commands.NoPrivateMessage):
-        await ctx.respond(embed=GenericErrors.guild_only(ctx))
-
-    elif isinstance(error, commands.NotOwner):
-        await ctx.respond(embed=GenericErrors.owner_only(ctx))
-
-    elif isinstance(error, (commands.MissingRequiredArgument, commands.BadArgument)):
-        return await ctx.respond(embed=GenericErrors.bad_arg(ctx, error))
-
-    elif isinstance(error, (discord.CheckFailure, commands.CheckFailure)):
-        """subclasses of this exception"""
-        if isinstance(error, LumiExceptions.NotAllowedInChannel):
-            await ctx.respond(
-                content=f"You can only do that command in {error.command_channel.mention}.",
-                ephemeral=True,
+# --- Error Handling Functions ---
+async def handle_command_error(ctx, error):
+    error_handlers = {
+        commands.CommandNotFound: lambda: None,  # Ignore command not found
+        commands.CommandOnCooldown: lambda: ctx.respond(
+            embed=GenericErrors.command_on_cooldown(
+                ctx, format_cooldown(error.retry_after)
             )
+        ),
+        commands.MissingPermissions: lambda: ctx.respond(
+            embed=GenericErrors.missing_permissions(ctx)
+        ),
+        commands.BotMissingPermissions: lambda: ctx.respond(
+            embed=GenericErrors.bot_missing_permissions(ctx)
+        ),
+        commands.PrivateMessageOnly: lambda: ctx.respond(
+            embed=GenericErrors.private_message_only(ctx)
+        ),
+        commands.NoPrivateMessage: lambda: ctx.respond(
+            embed=GenericErrors.guild_only(ctx)
+        ),
+        commands.NotOwner: lambda: ctx.respond(embed=GenericErrors.owner_only(ctx)),
+        (commands.MissingRequiredArgument, commands.BadArgument): lambda: ctx.respond(
+            embed=GenericErrors.bad_arg(ctx, error)
+        ),
+    }
 
-        elif isinstance(error, LumiExceptions.BirthdaysDisabled):
-            await ctx.respond(embed=BdayErrors.birthdays_disabled(ctx))
+    for error_type, handler in error_handlers.items():
+        if isinstance(error, error_type):
+            return await handler()
 
-    else:
-        await ctx.respond(embed=GenericErrors.default_exception(ctx))
-        logger.error(
-            f"on_command_error: errors.command.{ctx.command.qualified_name} | user: {ctx.author.name}"
+    # --- LumiExceptions ---
+    if isinstance(error, LumiExceptions.NotAllowedInChannel):
+        return await ctx.respond(
+            content=f"You can only do that command in {error.command_channel.mention}.",
+            ephemeral=True,
         )
 
+    if isinstance(error, LumiExceptions.BirthdaysDisabled):
+        return await ctx.respond(embed=BdayErrors.birthdays_disabled(ctx))
 
-async def on_error(event: str, *args, **kwargs) -> None:
-    logger.exception(
-        f"on_error INFO: errors.event.{event} | '*args': {args} | '**kwargs': {kwargs}"
+    # --- Default Exception Handling ---
+    await ctx.respond(embed=GenericErrors.default_exception(ctx))
+    log_error(
+        f"on_command_error: errors.command.{ctx.command.qualified_name} | user: {ctx.author.name}"
     )
-    logger.exception(f"on_error EXCEPTION: {sys.exc_info()}")
+
+
+async def log_error(message):
+    logger.error(message)
+    logger.exception(f"EXCEPTION: {sys.exc_info()}")
     traceback.print_exc()
 
 
-class ErrorListener(Cog):
+def format_cooldown(seconds):
+    minutes = seconds // 60
+    seconds %= 60
+    return f"{int(minutes):02d}:{seconds:02d}"
+
+
+# --- ErrorListener Cog ---
+class ErrorListener(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    # @Cog.listener()
-    # async def on_command_error(self, ctx, error) -> None:
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if not isinstance(error, LumiExceptions.NotAllowedInChannel):
+            await handle_command_error(ctx, error)
 
-    #     # on a prefix command, don't send anything if channel check fails. (to prevent spam in non-bot channels)
-    #     # current issues with this: await ctx.trigger_typing() is still invoked for 10 seconds.
-    #     if not isinstance(error, LumiExceptions.NotAllowedInChannel):
-    #         await on_command_error(ctx, error)
+        log_message = f"{ctx.author.name} executed .{ctx.command.qualified_name} "
+        log_message += f" | guild: {ctx.guild.name} " if ctx.guild else "in DMs "
+        log_message += (
+            f"| FAILED: {f'{str(error)[:80]}...' if len(str(error)) > 80 else error}"
+        )
+        logger.warning(log_message)
 
-    #     log_msg = '%s executed .%s' % (ctx.author.name, ctx.command.qualified_name)
+    @commands.Cog.listener()  # Listen for slash command errors
+    async def on_application_command_error(self, ctx, error):
+        await self.on_command_error(ctx, error)  # Reuse the same handler
 
-    #     if ctx.guild is not None:
-    #         log_msg += f" | guild: {ctx.guild.name} "
-    #     else:
-    #         log_msg += " in DMs"
-
-    #     # make error shorter than full screen width
-    #     if len(str(error)) > 80:
-    #         error = str(error)[:80] + "..."
-    #     logger.warning(f"{log_msg} | FAILED: {error}")
-
-    # @Cog.listener()
-    # async def on_application_command_error(self, ctx, error) -> None:
-    #     await on_command_error(ctx, error)
-    #     log_msg = '%s executed /%s' % (ctx.author.name, ctx.command.qualified_name)
-
-    #     if ctx.guild is not None:
-    #         log_msg += f" | guild: {ctx.guild.name} "
-    #     else:
-    #         log_msg += " in DMs"
-
-    #     # make error shorter than full screen width
-    #     if len(str(error)) > 80:
-    #         error = str(error)[:80] + "..."
-    #     logger.warning(f"{log_msg} | FAILED: {error}")
-
-    # @Cog.listener()
-    # async def on_error(self, event: str, *args, **kwargs) -> None:
-    #     await on_error(event, *args, **kwargs)
+    @commands.Cog.listener()
+    async def on_error(self, event, *args, **kwargs):
+        log_error(
+            f"on_error INFO: errors.event.{event} | '*args': {args} | '**kwargs': {kwargs}"
+        )
 
 
+# --- Cog Setup ---
 def setup(client):
     client.add_cog(ErrorListener(client))
