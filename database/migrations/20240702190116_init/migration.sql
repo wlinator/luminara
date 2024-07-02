e6f6ff323197603fa5976858af5f6db4ffd2b6b8
wlinator <git@wlinator.org>
2024-07-02 21:06:19 +0200

    Update to use sqlite

--- database/migrations/20240702190116_init/migration.sql
+++ database/migrations/20240702190116_init/migration.sql
@@ -4,68 +4,58 @@ CREATE TABLE "xp" (
     "guild_id" BIGINT NOT NULL,
     "user_xp" INTEGER NOT NULL,
     "user_level" INTEGER NOT NULL,
-    "cooldown" DOUBLE PRECISION,
+    "cooldown" REAL,
 
-    CONSTRAINT "xp_pkey" PRIMARY KEY ("user_id","guild_id")
+    PRIMARY KEY ("user_id", "guild_id")
 );
 
 -- CreateTable
 CREATE TABLE "currency" (
-    "user_id" BIGINT NOT NULL,
-    "balance" BIGINT NOT NULL,
-
-    CONSTRAINT "currency_pkey" PRIMARY KEY ("user_id")
+    "user_id" BIGINT NOT NULL PRIMARY KEY,
+    "balance" BIGINT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "blackjack" (
-    "id" SERIAL NOT NULL,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "user_id" BIGINT,
     "is_won" BOOLEAN,
     "bet" BIGINT,
     "payout" BIGINT,
     "hand_player" TEXT,
-    "hand_dealer" TEXT,
-
-    CONSTRAINT "blackjack_pkey" PRIMARY KEY ("id")
+    "hand_dealer" TEXT
 );
 
 -- CreateTable
 CREATE TABLE "slots" (
-    "id" SERIAL NOT NULL,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "user_id" BIGINT,
     "is_won" BOOLEAN,
     "bet" BIGINT,
     "payout" BIGINT,
     "spin_type" TEXT,
-    "icons" TEXT,
-
-    CONSTRAINT "slots_pkey" PRIMARY KEY ("id")
+    "icons" TEXT
 );
 
 -- CreateTable
 CREATE TABLE "dailies" (
-    "id" SERIAL NOT NULL,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "user_id" BIGINT,
     "amount" BIGINT,
     "claimed_at" TEXT,
-    "streak" INTEGER,
-
-    CONSTRAINT "dailies_pkey" PRIMARY KEY ("id")
+    "streak" INTEGER
 );
 
 -- CreateTable
 CREATE TABLE "item" (
-    "id" SERIAL NOT NULL,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT,
     "display_name" TEXT,
     "description" TEXT,
     "image_url" TEXT,
     "emote_id" BIGINT,
     "quote" TEXT,
-    "type" TEXT,
-
-    CONSTRAINT "item_pkey" PRIMARY KEY ("id")
+    "type" TEXT
 );
 
 -- CreateTable
@@ -74,21 +64,22 @@ CREATE TABLE "inventory" (
     "item_id" INTEGER NOT NULL,
     "quantity" INTEGER NOT NULL,
 
-    CONSTRAINT "inventory_pkey" PRIMARY KEY ("user_id","item_id")
+    PRIMARY KEY ("user_id", "item_id"),
+    CONSTRAINT "inventory_item_id_fkey" FOREIGN KEY ("item_id") REFERENCES "item" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "birthdays" (
     "user_id" BIGINT NOT NULL,
     "guild_id" BIGINT NOT NULL,
-    "birthday" TIMESTAMP(3),
+    "birthday" DATETIME,
 
-    CONSTRAINT "birthdays_pkey" PRIMARY KEY ("user_id","guild_id")
+    PRIMARY KEY ("user_id", "guild_id")
 );
 
 -- CreateTable
 CREATE TABLE "guild_config" (
-    "guild_id" BIGINT NOT NULL,
+    "guild_id" BIGINT NOT NULL PRIMARY KEY,
     "prefix" TEXT,
     "birthday_channel_id" BIGINT,
     "command_channel_id" BIGINT,
@@ -100,9 +91,7 @@ CREATE TABLE "guild_config" (
     "boost_image_url" TEXT,
     "level_channel_id" BIGINT,
     "level_message" TEXT,
-    "level_message_type" INTEGER NOT NULL DEFAULT 1,
-
-    CONSTRAINT "guild_config_pkey" PRIMARY KEY ("guild_id")
+    "level_message_type" INTEGER NOT NULL DEFAULT 1
 );
 
 -- CreateTable
@@ -112,18 +101,13 @@ CREATE TABLE "level_rewards" (
     "role_id" BIGINT,
     "persistent" BOOLEAN,
 
-    CONSTRAINT "level_rewards_pkey" PRIMARY KEY ("guild_id","level")
+    PRIMARY KEY ("guild_id", "level")
 );
 
 -- CreateTable
 CREATE TABLE "blacklist_user" (
-    "user_id" BIGINT NOT NULL,
+    "user_id" BIGINT NOT NULL PRIMARY KEY,
     "reason" TEXT,
-    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "active" BOOLEAN NOT NULL DEFAULT true,
-
-    CONSTRAINT "blacklist_user_pkey" PRIMARY KEY ("user_id")
+    "timestamp" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "active" BOOLEAN NOT NULL DEFAULT true
 );
-
--- AddForeignKey
-ALTER TABLE "inventory" ADD CONSTRAINT "inventory_item_id_fkey" FOREIGN KEY ("item_id") REFERENCES "item"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
