24f7f403574a78747a083f01c1131434eddb9a18
wlinator <dokimakimaki@gmail.com>
2024-07-10 22:11:12 +0200

    Refactor trigger column name in custom_reactions table

--- db/migrations/v2_5_9_reactions.sql
+++ db/migrations/v2_5_9_reactions.sql
@@ -1,7 +1,7 @@
 -- Create a table to store custom reactions
 CREATE TABLE custom_reactions (
     id SERIAL PRIMARY KEY,  -- Unique identifier for each custom reaction
-    trigger TEXT NOT NULL,  -- The text that triggers the custom reaction
+    trigger_text TEXT NOT NULL,  -- The text that triggers the custom reaction
     response TEXT,  -- The response text for the custom reaction (nullable for emoji reactions)
     emoji TEXT,  -- The emoji for the custom reaction (nullable for text responses)
     is_emoji BOOLEAN DEFAULT FALSE,  -- Indicates if the reaction is a discord emoji reaction
@@ -12,10 +12,10 @@ CREATE TABLE custom_reactions (
     usage_count INT DEFAULT 0,  -- The number of times a custom reaction has been used
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- Timestamp when the custom reaction was created
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- Timestamp when the custom reaction was last updated
-    CONSTRAINT unique_trigger_guild UNIQUE (trigger, guild_id)  -- Ensure that the combination of trigger, guild_id, and is_full_match is unique
+    CONSTRAINT unique_trigger_guild UNIQUE (trigger_text, guild_id)  -- Ensure that the combination of trigger_text, guild_id, and is_full_match is unique
 );
 
 -- Create indexes to speed up lookups
 CREATE INDEX idx_custom_reactions_guild_id ON custom_reactions(guild_id);
 CREATE INDEX idx_custom_reactions_creator_id ON custom_reactions(creator_id);
-CREATE INDEX idx_custom_reactions_trigger ON custom_reactions(trigger);
+CREATE INDEX idx_custom_reactions_trigger_text ON custom_reactions(trigger_text);
--- handlers/reaction_handler.py
+++ handlers/reaction_handler.py
@@ -1,11 +1,8 @@
-import random
-import asyncio
-from typing import List, Dict, Any
-
-from discord.ext.commands import Cog
 from discord import Message
+from discord.ext.commands import Cog
+from loguru import logger
 
-from config.parser import JsonCache
+from services.reactions_service import CustomReactionsService
 from services.blacklist_service import BlacklistUserService
 
 
@@ -13,68 +10,53 @@ class ReactionHandler:
     """
     Handles reactions to messages based on predefined triggers and responses.
     """
-    
+
     def __init__(self, client, message: Message) -> None:
-        self.reactions: Dict[str, Any] = JsonCache.read_json("reactions")
-        self.eightball: List[str] = self.reactions["eightball"]
-        self.full_response: Dict[str, str] = self.reactions["full_content_responses"]
-        self.partial_react: Dict[str, str] = self.reactions["partial_content_reactions"]
+        self.client = client
         self.message: Message = message
         self.content: str = self.message.content.lower()
-        self.client = client
-    
+        self.reaction_service = CustomReactionsService()
+
     async def run_all_checks(self) -> None:
         """
         Runs all checks for reactions and responses.
         """
-        await asyncio.gather(
-            self.check_eightball(self.eightball),
-            self.check_full_response(),
-            self.react()
-        )
-    
-    async def check_eightball(self, choices: List[str]) -> None:
-        """
-        Checks if the message is a question directed at Lumi and responds with a random choice.
-        
-        :param choices: List of possible responses.
-        """
-        if (self.content.startswith("lumi ") or self.content.startswith("lumi, ")) and self.content.endswith("?"):
-            response: str = random.choice(choices)
-            await self.message.reply(content=response)
-    
-    async def check_full_response(self) -> None:
-        """
-        Checks if the message content matches any full content triggers and responds accordingly.
-        """
-        for trigger, response in self.full_response.items():
-            if trigger.lower() == self.content:
-                await self.message.reply(response)
+        guild_id = self.message.guild.id if self.message.guild else None
 
-    async def react(self) -> None:
-        """
-        Adds reactions to the message based on partial content triggers.
-        """
-        ctx = await self.client.get_context(self.message)
-        for trigger, emoji in self.partial_react.items():
-            if trigger.lower() in self.content:
-                emoji = await self.client.convert_to_emoji(ctx, emoji)
-                if emoji:
-                    await self.message.add_reaction(emoji)
+        if guild_id:
+            reaction = await self.reaction_service.find_trigger(guild_id, self.content)
+            if reaction:
+                processed = False
+                try:
+                    if reaction["type"] == "text":
+                        await self.message.reply(reaction["response"])
+                        processed = True
+                    elif reaction["type"] == "emoji":
+                        await self.message.add_reaction(reaction["response"])
+                        processed = True
+                except Exception as e:
+                    logger.warning(f"Failed to process reaction: {e}")
+
+                if processed:
+                    await self.reaction_service.increment_reaction_usage(
+                        int(reaction["id"])
+                    )
 
 
 class ReactionListener(Cog):
     def __init__(self, client) -> None:
         self.client = client
 
-    @Cog.listener('on_message')
+    @Cog.listener("on_message")
     async def reaction_listener(self, message: Message) -> None:
         """
         Listens for new messages and processes them if the author is not a bot and not blacklisted.
-        
+
         :param message: The message to process.
         """
-        if not message.author.bot and not BlacklistUserService.is_user_blacklisted(message.author.id):
+        if not message.author.bot and not BlacklistUserService.is_user_blacklisted(
+            message.author.id
+        ):
             await ReactionHandler(self.client, message).run_all_checks()
 
 
