4d13a62a869047c981ab8474a2f60d6f8d857667
wlinator <dokimakimaki@gmail.com>
2024-07-14 11:07:15 -0400

    Use EmbedBuilder in the rank command.

--- modules/levels/level.py
+++ modules/levels/level.py
@@ -1,18 +1,27 @@
-import discord
-
+from discord import Embed
+from discord.ext import commands
+from lib.constants import CONST
+from lib.embed_builder import EmbedBuilder
 from services.xp_service import XpService
 
 
-async def cmd(ctx):
-    xp_data = XpService(ctx.author.id, ctx.guild.id)
-    rank = xp_data.calculate_rank()
-    needed_xp_for_next_level = XpService.xp_needed_for_next_level(xp_data.level)
+async def rank(ctx: commands.Context) -> None:
+    xp_data: XpService = XpService(ctx.author.id, ctx.guild.id)
 
-    embed = discord.Embed(color=0xadcca6, title=f"Level {xp_data.level}")
-    embed.add_field(name=f"Progress to next level",
-                    value=XpService.generate_progress_bar(xp_data.xp, needed_xp_for_next_level), inline=False)
+    rank: str = str(xp_data.calculate_rank())
+    needed_xp_for_next_level: int = XpService.xp_needed_for_next_level(xp_data.level)
 
-    embed.set_footer(text=f"Server Rank: #{rank}")
-    embed.set_thumbnail(url=ctx.author.display_avatar)
+    embed: Embed = EmbedBuilder.create_success_embed(
+        ctx=ctx,
+        title=CONST.STRINGS["xp_level"].format(xp_data.level),
+        footer_text=CONST.STRINGS["xp_server_rank"].format(rank or "NaN"),
+        show_name=False,
+        thumbnail_url=ctx.author.display_avatar.url,
+    )
+    embed.add_field(
+        name=CONST.STRINGS["xp_progress"],
+        value=XpService.generate_progress_bar(xp_data.xp, needed_xp_for_next_level),
+        inline=False,
+    )
 
     await ctx.respond(embed=embed, content=ctx.author.mention)
