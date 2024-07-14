f6d6ceea3d0f91eaeb24cc43e3d65ee45a0f16bd
wlinator <git@wlinator.org>
2024-07-13 21:58:02 -0400

    Refactor backup module to improve error handling and use constants for environment variables

--- modules/misc/__init__.py
+++ modules/misc/__init__.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 import discord
 from discord.commands import SlashCommandGroup
@@ -9,17 +9,16 @@ from lib.embeds.info import MiscInfo
 from Client import LumiBot
 from modules.config import set_prefix
 from modules.misc import avatar, introduction, invite, backup, info, xkcd
-from typing import Optional
 
 
 class Misc(commands.Cog):
-    def __init__(self, client: LumiBot):
-        self.client = client
-        self.start_time = datetime.datetime.now()
+    def __init__(self, client: LumiBot) -> None:
+        self.client: LumiBot = client
+        self.start_time: datetime = datetime.now()
         self.do_backup.start()
 
     @tasks.loop(hours=1)
-    async def do_backup(self):
+    async def do_backup(self) -> None:
         await backup.backup()
 
     @bridge.bridge_command(
@@ -31,7 +30,7 @@ class Misc(commands.Cog):
     )
     @commands.guild_only()
     @checks.allowed_in_channel()
-    async def avatar(self, ctx, *, user: Optional[discord.Member] = None):
+    async def avatar(self, ctx: bridge.Context, *, user: discord.Member) -> None:
         return await avatar.get_avatar(ctx, user)
 
     @bridge.bridge_command(
@@ -41,8 +40,8 @@ class Misc(commands.Cog):
         help="Simple status check.",
     )
     @checks.allowed_in_channel()
-    async def ping(self, ctx):
-        return await ctx.respond(embed=MiscInfo.ping(ctx, self.client))
+    async def ping(self, ctx: bridge.Context) -> None:
+        await ctx.respond(embed=MiscInfo.ping(ctx, self.client))
 
     @bridge.bridge_command(
         name="uptime",
@@ -50,9 +49,9 @@ class Misc(commands.Cog):
         help="See how long Lumi has been online since his last update.",
     )
     @checks.allowed_in_channel()
-    async def uptime(self, ctx):
-        unix_timestamp = int(round(self.start_time.timestamp()))
-        return await ctx.respond(
+    async def uptime(self, ctx: bridge.Context) -> None:
+        unix_timestamp: int = int(round(self.start_time.timestamp()))
+        await ctx.respond(
             embed=MiscInfo.uptime(ctx, self.client, unix_timestamp)
         )
 
@@ -62,8 +61,8 @@ class Misc(commands.Cog):
         help="Generate a link to invite Lumi to your own server!",
     )
     @checks.allowed_in_channel()
-    async def invite_command(self, ctx):
-        return await invite.cmd(ctx)
+    async def invite_command(self, ctx: bridge.Context) -> None:
+        await invite.cmd(ctx)
 
     @bridge.bridge_command(
         name="prefix",
@@ -73,8 +72,8 @@ class Misc(commands.Cog):
     )
     @commands.guild_only()
     @checks.allowed_in_channel()
-    async def prefix_command(self, ctx):
-        return await set_prefix.get_cmd(ctx)
+    async def prefix_command(self, ctx: bridge.Context) -> None:
+        await set_prefix.get_cmd(ctx)
 
     @bridge.bridge_command(
         name="info",
@@ -83,9 +82,9 @@ class Misc(commands.Cog):
         help="Shows basic Lumi stats.",
     )
     @checks.allowed_in_channel()
-    async def info_command(self, ctx):
-        unix_timestamp = int(round(self.start_time.timestamp()))
-        return await info.cmd(self, ctx, unix_timestamp)
+    async def info_command(self, ctx: bridge.Context) -> None:
+        unix_timestamp: int = int(round(self.start_time.timestamp()))
+        await info.cmd(self, ctx, unix_timestamp)
 
     @bridge.bridge_command(
         name="introduction",
@@ -96,28 +95,28 @@ class Misc(commands.Cog):
         "can only be done in ONE server and only in Lumi's DMs.",
     )
     @commands.dm_only()
-    async def intro_command(self, ctx):
-        return await introduction.cmd(self, ctx)
+    async def intro_command(self, ctx: bridge.Context) -> None:
+        await introduction.cmd(self, ctx)
 
     """
     xkcd submodule - slash command only
     """
-    xkcd = SlashCommandGroup(
+    xkcd: SlashCommandGroup = SlashCommandGroup(
         "xkcd", "A web comic of romance, sarcasm, math, and language.", guild_only=False
     )
 
     @xkcd.command(name="latest", description="Get the latest xkcd comic.")
-    async def xkcd_latest(self, ctx):
-        return await xkcd.print_comic(ctx, latest=True)
+    async def xkcd_latest(self, ctx: bridge.Context) -> None:
+        await xkcd.print_comic(ctx, latest=True)
 
     @xkcd.command(name="random", description="Get a random xkcd comic.")
-    async def xkcd_random(self, ctx):
-        return await xkcd.print_comic(ctx)
+    async def xkcd_random(self, ctx: bridge.Context) -> None:
+        await xkcd.print_comic(ctx)
 
     @xkcd.command(name="search", description="Search for a xkcd comic by ID.")
-    async def xkcd_search(self, ctx, *, id: int):
-        return await xkcd.print_comic(ctx, number=id)
+    async def xkcd_search(self, ctx: bridge.Context, *, id: int) -> None:
+        await xkcd.print_comic(ctx, number=id)
 
 
-def setup(client):
+def setup(client: LumiBot) -> None:
     client.add_cog(Misc(client))
