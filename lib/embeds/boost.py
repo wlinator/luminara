929cc6d4532ffa92174a7bc85defafa806f51dbb
wlinator <dokimakimaki@gmail.com>
2024-07-15 08:34:24 -0400

    refactor: Improve Boost class embed formatting
    Refactor the Boost class in the `boost.py` file to improve the formatting of the embed. The `description` field now includes the name of the member who boosted the server. Additionally, the `set_footer` method has been updated to include the total number of server boosts.

--- lib/embeds/boost.py
+++ lib/embeds/boost.py
@@ -10,7 +10,7 @@ class Boost:
         embed = discord.Embed(
             color=discord.Color.nitro_pink(),
             title="New Booster",
-            description=f"Thanks for boosting, **{member.name}**!!"
+            description=f"Thanks for boosting, **{member.name}**!!",
         )
 
         if template:
@@ -19,7 +19,9 @@ class Boost:
 
         embed.set_author(name=member.name, icon_url=member.display_avatar)
         embed.set_image(url=image_url if image_url else CONST.BOOST_ICON)
-        embed.set_footer(text=f"Total server boosts: {member.guild.premium_subscription_count}",
-                         icon_url=CONST.EXCLAIM_ICON)
+        embed.set_footer(
+            text=f"Total server boosts: {member.guild.premium_subscription_count}",
+            icon_url=CONST.EXCLAIM_ICON,
+        )
 
         return embed
