dd3ae5dfd5cdbbdbdafb4b8e121d969ee7e7360e
wlinator <git@wlinator.org>
2024-07-15 10:01:30 -0400

    refactor: Improve error embed formatting

--- lib/exceptions/LumiExceptions.py
+++ lib/exceptions/LumiExceptions.py
@@ -15,7 +15,18 @@ class BirthdaysDisabled(commands.CheckFailure):
     """
     Raised when the birthdays module is disabled in ctx.guild.
     """
+
     pass
+
+
+class LumiException(commands.CommandError):
+    """
+    A generic exception to raise for quick error handling.
+    """
+
+    def __init__(self, message="An error occurred."):
+        self.message = message
+        super().__init__(message)
 
 
 class UserHierarchy(discord.DiscordException):
