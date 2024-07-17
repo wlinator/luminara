a22a4d0dca4d131bd1bca534ac6e4ba5ffa9c260
wlinator <dokimakimaki@gmail.com>
2024-07-17 05:43:29 -0400

    Refactor LumiBot class to add bot admin logging

--- lib/constants.py
+++ lib/constants.py
@@ -16,11 +16,7 @@ class Constants:
     INSTANCE: Optional[str] = os.environ.get("INSTANCE", None)
 
     OWNER_IDS: Optional[Set[int]] = (
-        {
-            int(id.strip())
-            for id in os.environ.get("OWNER_IDS", "").split(",")
-            if id.strip()
-        }
+        {int(id.strip()) for id in os.environ.get("OWNER_IDS", "").split(",") if id}
         if os.environ.get("OWNER_IDS")
         else None
     )
