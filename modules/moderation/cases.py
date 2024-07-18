16ae18f59e214185036df547a603bb747a0d960e
wlinator <dokimakimaki@gmail.com>
2024-07-18 12:24:57 -0400

    refactor: Add error message for case not found

--- modules/moderation/cases.py
+++ modules/moderation/cases.py
@@ -1,5 +1,8 @@
-from discord.ext import pages
+from discord.ext.commands import UserConverter
 from services.moderation.case_service import CaseService
+from modules.moderation.utils.case_embed import create_case_embed
+from lib.embed_builder import EmbedBuilder
+from lib.constants import CONST
 
 case_service = CaseService()
 
@@ -8,47 +11,58 @@ async def view_case_by_number(ctx, guild_id: int, case_number: int):
     case = case_service.fetch_case_by_guild_and_number(guild_id, case_number)
 
     if not case:
-        return await ctx.send("No case found with that ID.")
+        embed = EmbedBuilder.create_error_embed(
+            ctx,
+            author_text=CONST.STRINGS["error_no_case_found_author"],
+            description=CONST.STRINGS["error_no_case_found_description"],
+        )
+        return await ctx.respond(embed=embed)
 
-    await ctx.send(
-        f"Case {case['case_number']}: {case['action_type']} - {case['reason']}",
+    target = await UserConverter().convert(ctx, str(case["target_id"]))
+    embed = create_case_embed(
+        ctx,
+        target,
+        case["case_number"],
+        case["action_type"],
+        case["reason"],
     )
+    await ctx.respond(embed=embed)
 
 
-async def view_all_cases_in_guild(ctx, guild_id: int):
-    cases = case_service.fetch_all_cases_in_guild(guild_id)
+# async def view_all_cases_in_guild(ctx, guild_id: int):
+#     cases = case_service.fetch_all_cases_in_guild(guild_id)
 
-    if not cases:
-        return await ctx.send("No cases found for this guild.")
+#     if not cases:
+#         return await ctx.send("No cases found for this guild.")
 
-    pages_list = [
-        f"Case {case['case_number']}: {case['action_type']} - {case['reason']}"
-        for case in cases
-    ]
-    paginator = pages.Paginator(pages=pages_list, loop_pages=True)
-    await paginator.send(ctx)
-
-
-async def view_all_cases_by_mod(ctx, guild_id: int, mod_id: int):
-    cases = case_service.fetch_all_cases_by_mod(guild_id, mod_id)
-
-    if not cases:
-        return await ctx.send("No cases found for this moderator in this guild.")
-
-    pages_list = [
-        f"Case {case['case_number']}: {case['action_type']} - {case['reason']}"
-        for case in cases
-    ]
-    paginator = pages.Paginator(pages=pages_list, loop_pages=True)
-    await paginator.send(ctx)
+#     pages_list = [
+#         f"Case {case['case_number']}: {case['action_type']} - {case['reason']}"
+#         for case in cases
+#     ]
+#     paginator = pages.Paginator(pages=pages_list, loop_pages=True)
+#     await paginator.send(ctx)
 
 
-async def edit_case_reason(ctx, guild_id: int, case_number: int, new_reason: str):
-    changes = {"reason": new_reason}
-    case_service.edit_case(guild_id, case_number, changes)
-    await ctx.respond(f"Case {case_number} reason updated to: {new_reason}")
+# async def view_all_cases_by_mod(ctx, guild_id: int, mod_id: int):
+#     cases = case_service.fetch_all_cases_by_mod(guild_id, mod_id)
+
+#     if not cases:
+#         return await ctx.send("No cases found for this moderator in this guild.")
+
+#     pages_list = [
+#         f"Case {case['case_number']}: {case['action_type']} - {case['reason']}"
+#         for case in cases
+#     ]
+#     paginator = pages.Paginator(pages=pages_list, loop_pages=True)
+#     await paginator.send(ctx)
 
 
-async def close_case(ctx, guild_id: int, case_number: int):
-    case_service.close_case(guild_id, case_number)
-    await ctx.respond(f"Case {case_number} has been closed.")
+# async def edit_case_reason(ctx, guild_id: int, case_number: int, new_reason: str):
+#     changes = {"reason": new_reason}
+#     case_service.edit_case(guild_id, case_number, changes)
+#     await ctx.respond(f"Case {case_number} reason updated to: {new_reason}")
+
+
+# async def close_case(ctx, guild_id: int, case_number: int):
+#     case_service.close_case(guild_id, case_number)
+#     await ctx.respond(f"Case {case_number} has been closed.")
