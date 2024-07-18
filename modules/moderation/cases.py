17b4816fe90c20ec0cbd009c75e943cd6c8119f5
wlinator <git@wlinator.org>
2024-07-18 16:32:07 -0400

    refactor: Update case creation embed fields with dynamic values

--- modules/moderation/cases.py
+++ modules/moderation/cases.py
@@ -38,12 +38,21 @@ async def view_all_cases_in_guild(ctx, guild_id: int):
     cases = case_service.fetch_cases_by_guild(guild_id)
 
     if not cases:
-        return await ctx.respond("This server doesn't have any mod cases yet.")
+        embed = EmbedBuilder.create_error_embed(
+            ctx,
+            author_text=CONST.STRINGS["case_guild_no_cases_author"],
+            description=CONST.STRINGS["case_guild_no_cases"],
+        )
+        return await ctx.respond(embed=embed)
 
     pages_list = []
     for i in range(0, len(cases), 10):
         chunk = cases[i : i + 10]
-        embed = create_case_list_embed(ctx, chunk, "All Cases in this Server")
+        embed = create_case_list_embed(
+            ctx,
+            chunk,
+            CONST.STRINGS["case_guild_cases_author"],
+        )
         pages_list.append(embed)
 
     paginator = pages.Paginator(pages=pages_list)
@@ -54,7 +63,12 @@ async def view_all_cases_by_mod(ctx, guild_id: int, moderator: discord.User):
     cases = case_service.fetch_cases_by_moderator(guild_id, moderator.id)
 
     if not cases:
-        return await ctx.respond("This user has not handled any cases in this server.")
+        embed = EmbedBuilder.create_error_embed(
+            ctx,
+            author_text=CONST.STRINGS["case_mod_no_cases_author"],
+            description=CONST.STRINGS["case_mod_no_cases"],
+        )
+        return await ctx.respond(embed=embed)
 
     pages_list = []
     for i in range(0, len(cases), 10):
@@ -62,7 +76,7 @@ async def view_all_cases_by_mod(ctx, guild_id: int, moderator: discord.User):
         embed = create_case_list_embed(
             ctx,
             chunk,
-            f"Cases by Moderator ({moderator.name})",
+            CONST.STRINGS["case_mod_cases_author"].format(moderator.name),
         )
         pages_list.append(embed)
 
