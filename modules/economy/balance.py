af7f3901fc5adfd66d1ce35f59dafcab2ff13576
wlinator <git@wlinator.org>
2024-08-22 23:02:02 +0200

    refactor balance command

--- modules/economy/balance.py
+++ modules/economy/balance.py
@@ -1,18 +1,22 @@
-import discord
+from discord.ext import commands
 
 from services.currency_service import Currency
+from lib.constants import CONST
+from lib.embed_builder import EmbedBuilder
 
 
-async def cmd(ctx):
-    # Currency handler
+async def cmd(ctx: commands.Context[commands.Bot]) -> None:
     ctx_currency = Currency(ctx.author.id)
-
     balance = Currency.format(ctx_currency.balance)
 
-    embed = discord.Embed(
-        color=discord.Color.embed_background(),
-        description=f"**Cash**: ${balance}",
+    embed = EmbedBuilder.create_success_embed(
+        ctx,
+        author_text=CONST.STRINGS["balance_author"].format(ctx.author.name),
+        author_icon_url=ctx.author.display_avatar.url,
+        description=CONST.STRINGS["balance_cash"].format(balance),
+        footer_text=CONST.STRINGS["balance_footer"],
+        show_name=False,
+        hide_timestamp=True,
     )
-    embed.set_author(name=f"{ctx.author.name}'s wallet", icon_url=ctx.author.avatar.url)
 
     await ctx.respond(embed=embed)
