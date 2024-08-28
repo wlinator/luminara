fc2d06a944b3910d93a27b292b66d8acd8aeb853
wlinator <git@wlinator.org>
2024-08-28 10:33:31 -0400

    Add backup module

---
+++ modules/misc/backup.py
@@ -0,0 +1,87 @@
+import subprocess
+from datetime import datetime
+from typing import List, Optional
+
+from discord.ext import commands, tasks
+import dropbox
+from dropbox.files import FileMetadata
+from loguru import logger
+
+from lib.const import CONST
+
+# Initialize Dropbox client if instance is "main"
+_dbx: Optional[dropbox.Dropbox] = None
+if CONST.INSTANCE and CONST.INSTANCE.lower() == "main":
+    _app_key: Optional[str] = CONST.DBX_APP_KEY
+    _dbx_token: Optional[str] = CONST.DBX_TOKEN
+    _app_secret: Optional[str] = CONST.DBX_APP_SECRET
+
+    _dbx = dropbox.Dropbox(
+        app_key=_app_key,
+        app_secret=_app_secret,
+        oauth2_refresh_token=_dbx_token,
+    )
+
+
+async def create_db_backup() -> None:
+    if not _dbx:
+        raise ValueError("Dropbox client is not initialized")
+
+    backup_name: str = datetime.now().strftime("%Y-%m-%d_%H%M") + "_lumi.sql"
+    command: str = (
+        f"mariadb-dump --user={CONST.MARIADB_USER} --password={CONST.MARIADB_PASSWORD} "
+        f"--host=db --single-transaction --all-databases > ./db/migrations/100-dump.sql"
+    )
+
+    subprocess.check_output(command, shell=True)
+
+    with open("./db/migrations/100-dump.sql", "rb") as f:
+        _dbx.files_upload(f.read(), f"/{backup_name}")
+
+
+async def backup_cleanup() -> None:
+    if not _dbx:
+        raise ValueError("Dropbox client is not initialized")
+
+    result = _dbx.files_list_folder("")
+
+    all_backup_files: List[str] = [
+        entry.name
+        for entry in result.entries  # type: ignore
+        if isinstance(entry, FileMetadata)
+    ]
+
+    for file in sorted(all_backup_files)[:-48]:
+        _dbx.files_delete_v2(f"/{file}")
+
+
+async def backup() -> None:
+    if CONST.INSTANCE and CONST.INSTANCE.lower() == "main":
+        logger.debug("Backing up the database.")
+        try:
+            await create_db_backup()
+            await backup_cleanup()
+            logger.debug("Backup successful.")
+        except Exception as error:
+            logger.error(f"Backup failed: {error}")
+    else:
+        logger.debug('No backup, instance not "MAIN".')
+
+
+class Backup(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot: commands.Bot = bot
+        self.do_backup.start()
+
+    @tasks.loop(hours=1)
+    async def do_backup(self) -> None:
+        await backup()
+
+    @commands.command()
+    async def backup(self, ctx: commands.Context[commands.Bot]) -> None:
+        await backup()
+        await ctx.send("Backup successful.")
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(Backup(bot))
